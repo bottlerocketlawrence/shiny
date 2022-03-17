9ef07519a6a1876ea8ac8d94e43d9c8923d7d436
Lawrence Tran <lawrence.tran@bottlerocketstudios.com>
2022-03-17 13:13:08 -0500

    Initial Commit

---
+++ Tests/ShinyTests/ShinyTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Shiny
+
+final class ShinyTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Shiny().text, "Hello, World!")
+    }
+}
